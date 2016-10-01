b3ad8b1d6bc2eeaff78c6d207f6cc1a37264a018
Lee Jia Cin <euphona@hotmail.com>
2016-10-01 22:52:34 +0800

    testing

--- src/janna/testing/controller/IndexController.java
+++ src/janna/testing/controller/IndexController.java
@@ -2,7 +2,6 @@ package janna.testing.controller;
 
 import java.io.Serializable;
 
-import javax.ejb.Init;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -17,10 +16,6 @@ public class IndexController implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String value = "testing123";
 
-	@Init
-	public void init(){
-		System.out.println("ok");
-	}
 	public String getValue() {
 		return value;
 	}
