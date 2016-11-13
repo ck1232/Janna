9b14b1d447569754d3991300a1242eb9937da237
Lee Jia Cin <euphona@hotmail.com>
2016-11-14 00:35:31 +0800

    User assign role with submodule listing done

--- WebContent/WEB-INF/jsp/roleassignment/assignRole.jsp
+++ WebContent/WEB-INF/jsp/roleassignment/assignRole.jsp
@@ -4,37 +4,78 @@
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!-- Content Wrapper. Contains page content -->
+<form id="backToListButton" method="get" action="<c:url value="/admin/listUser" />"></form>
 	<section class="content">
+		<c:url var="post_url" value="/admin/saveRoleToUser" />
+		<form:form id="saveRoleToUserForm" method="post" modelAttribute="user" action="${post_url }">
+		<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
     	<div class="row">
     		<div class="col-md-12">
     			<!--BOX-->
                 <div class="box">
-                	<!--BOX HEADER-->
                     <div class="box-header with-border">
                     	<h3 class="box-title">Selected User</h3>
                     </div>
-                    <!--FORM-->
-                    <form id="backToListButton" method="get" action="<c:url value="/admin/listUser" />"></form>
-                    <c:url var="post_url" value="/admin/saveRoleToUser" />
-                    <form:form id="saveRoleToUserForm" method="post" modelAttribute="user" action="${post_url }">
-                    	<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
-		              <div class="box-body">
-		              		<form:input path="id" type="hidden" id="id"/>
+	              	<div class="box-body">
+	              		<form:input path="id" type="hidden" id="id"/>
+	              		<div class="row">
 							<div class="col-sm-2">User id</div>
 							<div class="col-sm-10">${user.userid}</div>
-							<tiles:insertAttribute name = "datatable" />
-							
+						</div>
+				  	</div>
+				</div>
+				<!--BOX-->
+			  	<div class="box">
+					<div class="box-header with-border">
+                    	<h3 class="box-title">All Roles</h3>
+                    </div>
+	             	<div class="box-body">
+						<c:forEach items="${roleList}" var="role">
+							<c:if test="${roleList != null}">
+								<div class="row">
+									<div class="col-sm-2"><input type="checkbox" name="userRole" value="${role.roleid}" <c:if test="${role.checked == 'Y'}">checked</c:if>></div>
+									<div class="col-sm-10">${role.name}</div>
+								</div>
+							</c:if>
+							<c:if test="${roleList == null}">
+								<div class="row">
+									<div class="col-sm-12">No roles available</div>
+								</div>
+							</c:if>
+						</c:forEach>
 						<br/><br/>
-		            	<!--/.FORM-->
+	            		<div class="row">
 		            		<div class="col-sm-2"></div>
 							<div class="col-sm-10">
-							  <button name="saveRoleBtn" class="btn btn-primary" type="submit" form="saveRoleToUserForm">Save</button>
-			                  <button class="btn btn-default" type="submit" form="backToListButton"><i class="fa fa-remove"></i> Cancel</button>
+								<button name="saveRoleBtn" class="btn btn-primary" type="submit" form="saveRoleToUserForm">Save</button>
+							  	<button class="btn btn-default" type="submit" form="backToListButton"><i class="fa fa-remove"></i> Cancel</button>
 							</div>
 						</div>
-						</form:form>
-		              </div>
-		              <!-- /.box-body -->
-                </div>
+					</div>
+				</div>
+				<!--BOX-->
+			  	<div class="box">
+					<div class="box-header with-border">
+                    	<h3 class="box-title">All Submodules</h3>
+                    </div>
+	             	<div class="box-body">
+						<c:forEach items="${submoduleList}" var="submodule">
+							<c:if test="${submoduleList != null}">
+								<div class="row">
+									<div class="col-sm-5">${submodule.submodulename}</div>
+									<div class="col-sm-5">${submodule.permissiontype}</div>
+									<div class="col-sm-5"></div>
+								</div>
+							</c:if>
+							<c:if test="${roleList == null}">
+								<div class="row">
+									<div class="col-sm-12">No roles available</div>
+								</div>
+							</c:if>
+						</c:forEach>
+					</div>
+				</div>
+            </div>
     	</div>
+    	</form:form>
     </section>
--- src/com/JJ/controller/roleassignment/RoleAssignmentController.java
+++ src/com/JJ/controller/roleassignment/RoleAssignmentController.java
@@ -1,6 +1,9 @@
 package com.JJ.controller.roleassignment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +21,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.JJ.helper.GeneralUtils;
 import com.JJ.model.RolesToAssign;
+import com.JJ.model.Submodulepermission;
 import com.JJ.model.User;
 import com.JJ.model.UserRole;
+import com.JJ.service.permissionmanagement.PermissionManagementService;
 import com.JJ.service.roleassignment.RoleAssignmentService;
 import com.JJ.service.usermanagement.UserManagementService;
 
@@ -31,17 +37,72 @@ public class RoleAssignmentController {
 	
 	private UserManagementService userManagementService;
 	private RoleAssignmentService roleAssignmentService;
-	
+	private PermissionManagementService permissionManagementService;
 	@Autowired
-	public RoleAssignmentController(UserManagementService userManagementService, RoleAssignmentService roleAssignmentService) {
+	public RoleAssignmentController(UserManagementService userManagementService, RoleAssignmentService roleAssignmentService,
+			PermissionManagementService permissionManagementService) {
 		this.userManagementService = userManagementService;
 		this.roleAssignmentService = roleAssignmentService;
+		this.permissionManagementService = permissionManagementService;
 	}
 	
 	@RequestMapping(value = "/assignRole", method = RequestMethod.POST)
 	public String getUserToAssignRole(@RequestParam("assignRoleBtn") String id, Model model) {
 		User user = userManagementService.findById(new Integer(id));
+		List<RolesToAssign> rolesToAssign = roleAssignmentService.getRolesToAssign(id);
+		Map input = new HashMap();
+		List<String> rIdList = new ArrayList<String>();
+		if(rolesToAssign != null && rolesToAssign.size() > 0){
+			for(RolesToAssign role : rolesToAssign) {
+				if(role.getChecked().equals("Y")) {
+					rIdList.add(role.getRoleid());
+				}
+			}
+		}
+		input.put("list", rIdList);
+		List<Submodulepermission> smpList = permissionManagementService.getSubmodulePermissionByRoleIdList(input);
+		/*List<Submodulepermission> smpList = permissionManagementService.getSubmoduleByRole(rIdList);
+		List<Submodule> subModuleList = subModuleManagementService.getAllSubmodules();
+		Map<Integer, Submodule> submoduleMap = new HashMap<Integer, Submodule>();
+		if(subModuleList != null && subModuleList.size() > 0){
+			for(Submodule subModule : subModuleList){
+				submoduleMap.put(subModule.getId(), subModule);
+			}
+		}
+		if(smpList != null && smpList.size() > 0){
+			for(Submodulepermission smp : smpList){
+				Submodule submodule = submoduleMap.get(smp.getSubmoduleid());
+				if(submodule != null){
+					smp.setSubmodulename(submodule.getName());
+				}
+			}
+		}*/
+		
 		model.addAttribute("user", user);
+		model.addAttribute("roleList", rolesToAssign);
+		model.addAttribute("submoduleList", smpList);
+		return "assignRole";
+	}
+	
+	@RequestMapping(value = "/assignRole/{id}", method = RequestMethod.GET)
+	public String getUserToAssignRoleForRedirect(@PathVariable String id, Model model) {
+		User user = userManagementService.findById(new Integer(id));
+		List<RolesToAssign> rolesToAssign = roleAssignmentService.getRolesToAssign(id);
+		Map input = new HashMap();
+		List<String> rIdList = new ArrayList<String>();
+		if(rolesToAssign != null && rolesToAssign.size() > 0){
+			for(RolesToAssign role : rolesToAssign) {
+				if(role.getChecked().equals("Y")) {
+					rIdList.add(role.getRoleid());
+				}
+			}
+		}
+		input.put("list", rIdList);
+		List<Submodulepermission> smpList = permissionManagementService.getSubmodulePermissionByRoleIdList(input);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("roleList", rolesToAssign);
+		model.addAttribute("submoduleList", smpList);
 		return "assignRole";
 	}
 	
@@ -54,7 +115,7 @@ public class RoleAssignmentController {
 	
 	@RequestMapping(value = "/saveRoleToUser", method = RequestMethod.POST)
 	public String saveRoleToUser(@ModelAttribute("user") User user, 
-			@RequestParam(value="checkboxId", required=false) List<String> ids,
+			@RequestParam(value="userRole", required=false) List<String> ids,
 			Model model, final RedirectAttributes redirectAttributes) {
 		
 		logger.debug("saveRoleToUser() : user = " + user.getId());
@@ -74,7 +135,7 @@ public class RoleAssignmentController {
 		redirectAttributes.addFlashAttribute("css", "success");
 		redirectAttributes.addFlashAttribute("msg", "Role saved to user successfully!");
 		
-		return "redirect:listUser";
+		return "redirect:assignRole/"+user.getId();
 	}
 	
 	
