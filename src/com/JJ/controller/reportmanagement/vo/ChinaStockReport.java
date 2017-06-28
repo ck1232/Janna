581b96c074e994910569889745400d6152973f76
Choon Keat <behchoonkeat@hotmail.com>
2017-06-29 00:00:06 +0800

    resolve china stock report bug

--- src/com/JJ/controller/reportmanagement/vo/ChinaStockReport.java
+++ src/com/JJ/controller/reportmanagement/vo/ChinaStockReport.java
@@ -14,6 +14,10 @@ import com.JJ.controller.expensemanagement.VO.ExpenseVO;
 import com.JJ.controller.paymentmanagement.vo.PaymentDetailVO;
 import com.JJ.helper.ReportUtils;
 import com.JJ.helper.vo.ReportMapping;
+import com.JJ.lookup.ExpenseTypeLookup;
+import com.JJ.lookup.PaymentModeLookup;
+import com.JJ.lookup.vo.ExpenseTypeVO;
+import com.JJ.lookup.vo.PaymentModeVO;
 import com.JJ.service.expensemanagement.ExpenseManagementService;
 import com.JJ.service.paymentmanagement.PaymentManagementService;
 @Component
@@ -21,12 +25,17 @@ public class ChinaStockReport implements ReportInterface {
 	
 	private ExpenseManagementService expenseService;
 	private PaymentManagementService paymentService;
-	
+	private ExpenseTypeLookup expenseTypeLookup;
+	private PaymentModeLookup paymentModeLookup;
 	@Autowired
 	public ChinaStockReport(ExpenseManagementService expenseService,
-			PaymentManagementService paymentService) {
+			PaymentManagementService paymentService,
+			ExpenseTypeLookup expenseTypeLookup,
+			PaymentModeLookup paymentModeLookup) {
 		this.expenseService = expenseService;
 		this.paymentService = paymentService;
+		this.expenseTypeLookup = expenseTypeLookup;
+		this.paymentModeLookup = paymentModeLookup;
 	}
 
 
@@ -34,8 +43,9 @@ public class ChinaStockReport implements ReportInterface {
 	@Override
 	public Workbook exportReport(Workbook workbook, Date dateAsOf, Date endDate,
 			Map<String, Object> additionalMap) {
-		
-		List<ExpenseVO> dbVoList = expenseService.getAllExpense(dateAsOf, endDate, 15);
+		ExpenseTypeVO expenseVO = expenseTypeLookup.getExpenseTypeByValueMap().get("Stock(China)");
+		PaymentModeVO chequeModeVo = paymentModeLookup.getPaymentModeByValueMap().get("Cheque");
+		List<ExpenseVO> dbVoList = expenseService.getAllExpense(dateAsOf, endDate, expenseVO.getExpenseTypeId());
 		if(dbVoList != null && !dbVoList.isEmpty()) {
 			List<ExpenseReportVO> expenseReportList = new ArrayList<ExpenseReportVO>();
 			for(ExpenseVO vo : dbVoList) {
@@ -45,7 +55,7 @@ public class ChinaStockReport implements ReportInterface {
 					for(PaymentDetailVO paymentVO : paymentDetailList) {
 						expenseReportVo = new ExpenseReportVO();
 						expenseReportVo.setExpense(vo);
-						if(paymentVO.getPaymentMode() == 2 && 
+						if(paymentVO.getPaymentMode() == chequeModeVo.getPaymentModeId() && 
 								(paymentVO.getBounceChequeInd() != null && paymentVO.getBounceChequeInd().equals("Y")))
 							continue;
 						expenseReportVo.setPaymentDetail(paymentVO);
@@ -62,7 +72,7 @@ public class ChinaStockReport implements ReportInterface {
 			reportMapping.addTextMapping("Invoice", "expense.invoiceNo");
 			reportMapping.addTextMapping("Description", "expense.description");
 			reportMapping.addTextMapping("Mode of Payment", "paymentDetail.paymentModeString");
-			reportMapping.addMoneyMapping("Amount (RMB)", "paymentDetail.paymentAmt");
+			reportMapping.addChinaMoneyMapping("Amount (RMB)", "paymentDetail.paymentAmt");
 			reportMapping.addTextMapping("Cheque No.", "paymentDetail.chequeNum");
 			Sheet sheet = workbook.createSheet("RMB Purchase");
 			ReportUtils.writeData(sheet, expenseReportList, reportMapping, "");
